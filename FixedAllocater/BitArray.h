ea9c5e26721d678d84e8cf7bc9aa271660c644ca
Vikram <venkateshvikram96@gmail.com>
2020-01-02 13:24:42 -0700

    Added Unit test in main

---
+++ FixedAllocater/BitArray.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "MemorySystem.h"
+
+class BitArray
+{
+public:
+	
+	BitArray(size_t);
+	~BitArray();
+
+	int GetFirstSetBit();
+	void SetBit(const unsigned int index, const unsigned int value);
+
+	//new and delete operator overloads for 
+	//the BitArray class to use HeapAllocator instead of the FSA
+	void* operator new(size_t size)
+	{
+		return alloc(size);
+	}
+
+	void operator delete(void* ptr)
+	{
+		Free(ptr);
+	}
+
+private:
+	size_t size;
+	size_t * bits;
+};
